bb52a35a7516c939bb933c85658772ee7e1cce0c
svz71195 <[vladutescu@usm.lmu.de]>
2021-06-21 23:43:30 +0200

    Implemented Rshm and b-value calculation

--- galaxies.py
+++ galaxies.py
@@ -6,6 +6,8 @@ import sys
 import os
 from dataclasses import dataclass, field
 
+from numpy.core.numeric import indices
+
 
 try:
     import tqdm
@@ -31,34 +33,35 @@ class Magneticum:
     Collection of methods to retrieve data from Magneticum snapshots and subfind data.
     Requires python module 'g3read' from Antonio!!! You can find g3read at https://github.com/aragagnin/g3read 
     """
-    munit       = 1.e10
+    munit       = 1.e10 # Msun
     KPC_TO_CM   = 3.086e21
     KEV_TO_ERG  = 1.602e-9
     G           = 6.6726e-8
-    mp          = 1.672e-24
-    kB          = 1.38e-16
-    pc          = 3.086e18
+    mp          = 1.672623e-24
+    kB          = 1.380658e-16
+    pc          = 3.085678e18
     YR_TO_SEC   = 3.1557e7
 
     @staticmethod
     def get_halo_data( groupbase: str, GRNR: int ) -> tuple:
         """
-        returns tuple (center, Mstar, SFR, Mvir, Rvir, R25K, SZ) from group GRNR
+        returns tuple (center, Mstar, SFR, Mvir, Rvir, R25K, SVEL, SZ) from group GRNR
         """
         for halo in matcha.yield_haloes( groupbase, with_ids=True, ihalo_start=GRNR, ihalo_end=GRNR, blocks=('GPOS','MSTR','R25K','MVIR','RVIR') ):
             halo_center = halo["GPOS"]
-            halo_Mstar = halo["MSTR"] # stellar mass within R25K
+            halo_Mstar = halo["MSTR"][5] # stellar mass within R25K
             halo_R25K = halo["R25K"]
             halo_Mvir = halo["MVIR"]
             halo_Rvir = halo["RVIR"]
 
-            for subhalo in matcha.yield_subhaloes( groupbase, with_ids=True, halo_ids=halo['ids'], halo_goff=halo['GOFF'], ihalo=halo['ihalo'], blocks=('SSFR','SZ  ', 'REFF') ):
+            for subhalo in matcha.yield_subhaloes( groupbase, with_ids=True, halo_ids=halo['ids'], halo_goff=halo['GOFF'], ihalo=halo['ihalo'], blocks=('SPOS','SCM ','SVEL','SSFR','SZ  ') ):
+                subhalo_center = subhalo["SPOS"]
+                subhalo_vel = subhalo["SVEL"]
                 subhalo_SFR = subhalo["SSFR"]
                 subhalo_SZ = subhalo["SZ  "]
-                subhalo_Rshm = subhalo["REFF"]
                 break
 
-        return (halo_center, halo_Mstar, subhalo_SFR, halo_Mvir, halo_Rvir, halo_R25K, subhalo_SZ, subhalo_Rshm)
+        return (subhalo_center, halo_Mstar, subhalo_SFR, halo_Mvir, halo_Rvir, halo_R25K, subhalo_vel, subhalo_SZ)
 
     @staticmethod
     def get_halo_GRNR( groupbase, FSUB ):
@@ -68,18 +71,21 @@ class Magneticum:
             if FSUB < np.sum(halo_NSUB):
                 break
         j = 0
-        while ( np.sum(halo_NSUB[:j+1])-FSUB < 0 ):
+        while ( np.sum(halo_NSUB[:j+1])-FSUB <= 0 ):
             j = j+1
         return j
 
     @staticmethod
     def halo_gas(sb, ce, rad):
-        return g3.read_particles_in_box(snapbase=sb, center=ce, d=rad, blocks=["POS ", "VEL ", "MASS", "RHO ", "NH  ", "NE  ", "SFR ", "Zs ", "HSML"], ptypes=0)
+        return g3.read_particles_in_box(snap_file_name=sb, center=ce, d=rad, blocks=["POS ", "VEL ", "MASS", "RHO ", "NH  ", "NE  ", "SFR ", "Zs  ", "HSML"], ptypes=0)
 
     @staticmethod
     def halo_stars(sb, ce, rad):
-        return g3.read_particles_in_box(snapbase=sb, center=ce, d=rad, blocks=["POS ", "VEL ", "MASS", "iM  ", "AGE ", "Zs ", "HSMS"], ptypes=4)
+        return g3.read_particles_in_box(snap_file_name=sb, center=ce, d=rad, blocks=["POS ", "VEL ", "MASS", "iM  ", "AGE ", "Zs  ", "HSMS"], ptypes=4)
     
+    @staticmethod
+    def spec_ang_mom(mass, pos, vel):
+        return np.sum( np.cross( (mass*pos.T).T, vel, axis=1 ), axis=0 ) / np.sum(mass) 
 
     ### Cosmology
     def load_FlatLCDM(self):
@@ -118,6 +124,47 @@ class Magneticum:
         """
         return self.lum_dist(a) / a / a
 
+    def pos_to_phys(self, pos):
+        return pos * self.aa_c / self.h
+
+    def vel_to_phys(self, vel):
+        return vel * np.sqrt(self.aa_c)
+
+    def mass_to_phys(self, mass):
+        return mass * self.munit / self.h
+    
+    @staticmethod
+    def find_COM(pos, vel, mass, outer):
+        steps = np.arange(1,int(outer)+1)
+        pCOM = np.zeros(3)
+        vCOM = np.zeros(3)
+        for rad in reversed(steps):
+            rr = np.linalg.norm(pos,axis=1) # length of each postition vector
+            vv = np.linalg.norm(vel,axis=1) # length of each velocity vector
+            range_frac = .9
+            ii = np.sqrt(np.abs(vv))
+            n = len(vv)
+            vrange = np.abs( vv[ int( ii[int(range_frac*n-1)] ) ] )
+
+            kk = (rr <= rad) & (vv <= vrange)
+
+            p0 = np.array( [ np.sum( mass[kk]*pos.T[0][kk] ), np.sum( mass[kk]*pos.T[1][kk] ), np.sum( mass[kk]*pos.T[2][kk] ) ] ) / np.sum(mass[kk])
+            v0 = np.array( [ np.sum( mass[kk]*vel.T[0][kk] ), np.sum( mass[kk]*vel.T[1][kk] ), np.sum( mass[kk]*vel.T[2][kk] ) ] ) / np.sum(mass[kk])
+
+            pos -= p0
+            vel -= v0
+
+            pCOM += p0
+            vCOM += v0
+
+        return (pCOM, vCOM)
+
+
+
+
+
+
+
 
 
 @dataclass
@@ -137,6 +184,7 @@ class Galaxy(Magneticum):
     Mvir: float             = 1.
     Rvir: float             = 1.
     R25K: float             = 1.
+    Svel: float             = 1.
     sZ: float               = 1.
     redshift: float         = 0.
     
@@ -147,11 +195,10 @@ class Galaxy(Magneticum):
     bVal: float             = 1.
     
     ##--- Derived from PHOX .fits files
-    Xph_agn: dict           = {"PHE": np.zeros(3), "XPOS": np.zeros(3), "YPOS": np.zeros(3)}
-    Xph_gas: dict           = {"PHE": np.zeros(3), "XPOS": np.zeros(3), "YPOS": np.zeros(3)}
-    Xph_xrb: dict           = {"PHE": np.zeros(3), "XPOS": np.zeros(3), "YPOS": np.zeros(3)}
+    Xph_agn: dict           = field(default_factory=dict)
+    Xph_gas: dict           = field(default_factory=dict)
+    Xph_xrb: dict           = field(default_factory=dict)
     
-
     ##--- Derived from which Magneticum Box ---##
     groupbase: str          = "./Box4/uhr_test/groups_136/sub_136"
     snapbase: str           = "./Box4/uhr_test/snapdir_136/snap_136"
@@ -160,7 +207,7 @@ class Galaxy(Magneticum):
 
     @property
     def sFSUB(self):
-        return str(self.FSUB)
+        return f"{self.FSUB:06d}"
 
     @property
     def load(self):
@@ -190,10 +237,13 @@ class Galaxy(Magneticum):
         if has_tqdm:
             with tqdm.tqdm(total=6, file=sys.stdout) as pbar:
                 
+                self.load_FlatLCDM()
+                pbar.update(1)
+
                 self.set_GRNR
                 pbar.update(1)
 
-                self.center, self.Mstar, self.SFR, self.Mvir, self.Rvir, self.R25K = self.get_halo_data(self.groupbase, self.GRNR)
+                self.center, self.Mstar, self.SFR, self.Mvir, self.Rvir, self.R25K, self.Svel, self.sZ = self.get_halo_data(self.groupbase, self.GRNR)
                 pbar.update(1)
 
                 self.set_Rshm()
@@ -203,18 +253,17 @@ class Galaxy(Magneticum):
                 pbar.update(1)
 
                 if isinstance( self.Xph_base, str ):
-                    self.set_Xph( self.Xph_base )
+                    self.set_Xph
                 pbar.update(1)
 
-                self.load_FlatLCDM()
-                pbar.update(1)
         else:
+            self.load_FlatLCDM()
             self.set_GRNR( self.groupbase, self.FSUB)
             self.center, self.Mstar, self.SFR, self.Mvir, self.Rvir, self.R25K = self.get_halo_data(self.groupbase, self.FSUB)
             self.set_Rshm()
             self.set_bVal()
             self.set_Xph()
-            self.load_FlatLCDM()
+
 
     def set_groupbase(self, fp: str):
         if isinstance(fp, str):
@@ -273,19 +322,45 @@ class Galaxy(Magneticum):
         
         stars = self.get_stars
         mass = stars["MASS"]
-        st_rad = g3.to_spherical(stars["POS "], self.center).T[0]
-        st_mass = np.sum(mass[st_rad<=0.1*self.Rvir])
+        
+        st_rad = self.pos_to_phys(g3.to_spherical(stars["POS "], self.center).T[0])
+        st_mass = np.sum(mass[st_rad<=0.1*self.pos_to_phys(self.Rvir)])
+        # print(self.mass_to_phys(st_mass))
 
         # Approximate from below
         r = 0.
-        for dr in [1.,.1,.01,.001,.0001]:
-            while np.sum(mass[st_rad <= r]) <= .5*st_mass:
-                r += dr # Overestimates r -> substract one for next iteration
-            r -= dr        
-        return r
-    
+        for dr in [1., .1, .01, .001, 1.e-4, 1.e-5]:
+            while np.sum(mass[st_rad <= r+dr]) <= .5*st_mass:
+                r += dr   
+        self.Rshm = round(self.pos_to_phys(r),5)
+        self.Mstar = self.mass_to_phys(st_mass)
+
+        # jj = np.argsort(st_rad)
+        # mcur = 0.
+        # for k in range(len(st_rad)):
+        #     mcur += mass[jj[k]]
+        #     if mcur > .5*st_mass:
+        #         break
+        # r0 = self.pos_to_phys(st_rad[jj[k]])
+        # print(k, self.Rshm - r0)
+
     def set_bVal(self):
-        pass
+        """
+        Following Teklu+15, Schulze+18, cuts: b >= -4.35 -> LTG // b <= -4.73 -> ETG
+        """
+
+        stars = self.get_stars
+        mass = self.mass_to_phys(stars["MASS"])
+        pos = self.pos_to_phys(stars["POS "] - self.center)
+        rad = g3.to_spherical(pos, [0,0,0]).T[0]
+        vel = self.vel_to_phys( stars["VEL "] - self.Svel )
+        mask = ( rad <= 3.*self.Rshm )
+
+
+        st_mass_ph = np.sum(mass[mask]) 
+        # print(f"{st_mass_ph = :.2e}")
+
+        self.bVal = np.log10( np.linalg.norm(self.spec_ang_mom(mass[mask], pos[mask], vel[mask])) ) - 2./3. * np.log10(st_mass_ph)        
     
     @property
     def set_Xph(self):
@@ -297,31 +372,32 @@ class Galaxy(Magneticum):
         tbl_xrb = Table.read(fp_xrb)
         
         try:
-            self.Xph_gas["PHE"] = tbl_gas["ENERGY"].astype(float)
-            self.Xph_agn["PHE"] = tbl_agn["ENERGY"].astype(float)
-            self.Xph_xrb["PHE"] = tbl_xrb["ENERGY"].astype(float)
+            self.Xph_gas["PHE"] = np.array(tbl_gas["PHOTON_ENERGY"])
+            self.Xph_agn["PHE"] = np.array(tbl_agn["PHOTON_ENERGY"])
+            self.Xph_xrb["PHE"] = np.array(tbl_xrb["PHOTON_ENERGY"])
         except KeyError:
 
             try:
-                self.Xph_gas["PHE"] = tbl_gas["PHE"].astype(float)
-                self.Xph_agn["PHE"] = tbl_agn["PHE"].astype(float)
-                self.Xph_xrb["PHE"] = tbl_xrb["PHE"].astype(float)
+                self.Xph_gas["PHE"] = np.array(tbl_gas["PHE"])
+                self.Xph_agn["PHE"] = np.array(tbl_agn["PHE"])
+                self.Xph_xrb["PHE"] = np.array(tbl_xrb["PHE"])
             except KeyError:
-                raise KeyError("No column name called 'PHE' or 'ENERGY' found")
+                raise KeyError("No column name called 'PHE' or 'PHOTON_ENERGY' found")
         
         try:
-            self.Xph_gas["XPOS"] = tbl_gas["XPOS"].astype(float)
-            self.Xph_agn["XPOS"] = tbl_agn["XPOS"].astype(float)
-            self.Xph_xrb["XPOS"] = tbl_xrb["XPOS"].astype(float)
+            self.Xph_gas["XPOS"] = np.array(tbl_gas["POS_X"])
+            self.Xph_agn["XPOS"] = np.array(tbl_agn["POS_X"])
+            self.Xph_xrb["XPOS"] = np.array(tbl_xrb["POS_X"])
         except KeyError:
             raise KeyError("No coolumn name called 'XPOS' found")
 
         try:
-            self.Xph_gas["YPOS"] = tbl_gas["YPOS"].astype(float)
-            self.Xph_agn["YPOS"] = tbl_agn["YPOS"].astype(float)
-            self.Xph_xrb["YPOS"] = tbl_xrb["YPOS"].astype(float)
+            self.Xph_gas["YPOS"] = np.array(tbl_gas["POS_Y"])
+            self.Xph_agn["YPOS"] = np.array(tbl_agn["POS_Y"])
+            self.Xph_xrb["YPOS"] = np.array(tbl_xrb["POS_Y"])
         except KeyError:
             raise KeyError("No coolumn name called 'YPOS' found")
+        
 
     @property
     def get_gas(self):
@@ -340,10 +416,50 @@ class Galaxy(Magneticum):
         return self.get_stars()
 
     def add_luminosities(self):
-        _ = self.get_stars()
-        pass
-
+        stars = self.get_stars()
+        rad = g3.to_spherical( stars["POS "], self.center ).T[0]
+        iM = stars["iM  "]
+        age = self.age_part(stars["AGE "])
+        Zs = stars["Zs  "]
+        
     @classmethod
     def gal_dict_from_npy(cls, npy_obj):
         gal_dict = np.load(npy_obj).item()
         return gal_dict
+
+    def get_num_XRB(self, Tobs: float = 1., Aeff: float = 1.):
+        """
+        Returns tuple containing number of XRBs and array of luminosities
+        """
+        indx_pckg_end = np.where( np.diff(self.Xph_xrb) <= 0)[0]
+        numXRB = len(indx_pckg_end) + 1
+        lum = np.zeros(numXRB)
+        for i,ind in enumerate(indx_pckg_end):
+            lum[i] = self.calc_lum( self.Xph_xrb[ind,ind+1], Tobs, Aeff, self.lum_dist )
+
+        return (numXRB, lum)
+
+    @staticmethod
+    def calc_lum( phArr: np.ndarray, Tobs: float = 1., Aeff: float = 1., Dlum: float = 1. ):
+       return phArr * super().KEV_TO_ERG / Tobs / Aeff * Dlum * Dlum
+
+
+
+
+if __name__ == "__main__":
+    groupbase = "/home/lcladm/Studium/Masterarbeit/test/dorc/uhr_test/groups_136/sub_136"
+    snapbase = "/home/lcladm/Studium/Masterarbeit/test/dorc/uhr_test/snapdir_136/snap_136"
+    phbase = "/home/lcladm/Studium/Masterarbeit/R136_AGN_fix/fits/"
+
+    FSUB = g3.read_new(groupbase+".0", "FSUB",0)
+    print(FSUB[0],FSUB[1],FSUB[7])
+
+    x = Galaxy(1414,groupbase=groupbase,snapbase=snapbase,Xph_base=phbase)
+    y = Galaxy(6463,groupbase=groupbase,snapbase=snapbase,Xph_base=phbase)
+    z = Galaxy(10859,groupbase=groupbase,snapbase=snapbase,Xph_base=phbase)
+    x.load
+    y.load
+    z.load
+    print(x)
+    print(y)
+    print(z)
